f24d3273e20cce7101a8a6b1d08d320732e90c4e
Sidsol <sidsol@live.com>
2019-02-02 13:32:52 -0500

    Added Radar Graph Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Profile from "./components/Profile";
+import Radar from "./components/Radar";
 
 class App extends Component {
   render() {
     return (
+      <div>
       <Profile />
+      <Radar/>
+      </div>
     );
   }
 }

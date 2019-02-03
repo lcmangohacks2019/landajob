55e0f36ba4cb1245fcb3ef6a72969775241301ca
Sidsol <sidsol@live.com>
2019-02-03 01:26:06 -0500

    Created new visualization for Radar Component

--- src/components/Radar.js
+++ src/components/Radar.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import RadarChart from 'react-svg-radar-chart';
-import 'react-svg-radar-chart/build/css/index.css';
+// import React from 'react';
+import React, { Component } from 'react';
 import {
     Card, // CardImg, CardText, 
     CardBody,
@@ -9,50 +8,170 @@ import {
 } from 'reactstrap';
 import Flippy, { FrontSide, BackSide } from 'react-flippy';
 import Profile from "../components/Profile"
+//import './App.css';
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 
-class Radar extends React.Component {
-    render() {
-        const data = [
-            {
-                data: {
-                    Java: 0.7,
-                    IaaS: 0.9,
-                    PaaS: 0.9,
-                    RD: 1.0,
-                    JavaScript: .9
-                },
-                meta: { color: '#58FCEC' }
-            },
-            {
-                data: {
-                    Java: 0.3,
-                    IaaS: 0.2,
-                    PaaS: 0.1,
-                    RD: 1.0,
-                    JavaScript: .9
-                },
-                meta: { color: '#33ff77' }
-            }
-        ];
+am4core.useTheme(am4themes_animated);
 
-        const captions = {
-            // columns
-            Java: "Java",
-            IaaS: "IaaS",
-            PaaS: "PaaS",
-            RD: "RD",
-            JavaScript: "JavaScript"
-        };
 
-        return (
-            <Flippy
+// import RadarChart from 'react-svg-radar-chart';
+// import 'react-svg-radar-chart/build/css/index.css';
+
+// class Radar extends React.Component {
+//     render() {
+//         const data = [
+//             {
+//                 data: {
+//                     Java: 0.7,
+//                     IaaS: 0.9,
+//                     PaaS: 0.9,
+//                     RD: 1.0,
+//                     JavaScript: .9
+//                 },
+//                 meta: { color: '#58FCEC' }
+//             },
+//             {
+//                 data: {
+//                     Java: 0.3,
+//                     IaaS: 0.2,
+//                     PaaS: 0.1,
+//                     RD: 1.0,
+//                     JavaScript: .9
+//                 },
+//                 meta: { color: '#33ff77' }
+//             }
+//         ];
+
+//         const captions = {
+//             // columns
+//             Java: "Java",
+//             IaaS: "IaaS",
+//             PaaS: "PaaS",
+//             RD: "RD",
+//             JavaScript: "JavaScript"
+//         };
+
+//         return (
+
+//             <Flippy
+//                 flipOnHover={false} // default false
+//                 flipOnClick={true} // default false
+//                 flipDirection="horizontal" // horizontal or vertical
+//                 ref={(r) => this.flippy = r} // to use toggle method like this.flippy.toggle()
+//                 // if you pass isFlipped prop component will be controlled component.
+//                 // and other props, which will go to div
+//                 style={{}} /// these are optional style, it is not necessary
+//             >
+//                 <FrontSide
+//                     style={{
+//                         backgroundColor: '#ffffff',
+//                     }}
+//                 >
+//                     <div className="row">
+//                         <Row>
+//                             <Col sm="12">
+//                                 <Card>
+//                                     <RadarChart
+//                                         captions={captions
+//                                         }
+//                                         data={data
+//                                         }
+//                                         size={400}
+//                                     />
+//                                     <CardBody>
+//                                     </CardBody>
+//                                 </Card>
+//                             </Col>
+//                         </Row>
+//                     </div>
+//                 </FrontSide>
+//                 <BackSide
+//                     style={{ backgroundColor: '#ffffff' }}>
+//                     <Profile />
+//                 </BackSide>
+//             </Flippy>
+//         );
+//     }
+// }
+
+// export default Radar;
+
+/* Imports */
+
+
+am4core.useTheme(am4themes_animated);
+
+class Radar extends Component {
+  componentDidMount() {
+
+    let chart = am4core.create("chartdiv", am4charts.RadarChart);
+
+    chart.data = [{
+        "Skill": "Java",
+        "litres": 501
+      }, {
+        "Skill": "PaaS",
+        "litres": 301
+      }, {
+        "Skill": "SaaS",
+        "litres": 266
+      }, {
+        "Skill": "IaaS",
+        "litres": 165
+      }, {
+        "Skill": "React",
+        "litres": 139
+      }, {
+        "Skill": "Database",
+        "litres": 336
+      }, {
+        "Skill": "Git",
+        "litres": 290
+      }, {
+        "Skill": "Containers",
+        "litres": 325
+      }, {
+        "Skill": "CICD",
+        "litres": 40
+      }];
+
+  /* Create axes */
+  let series = chart.series.push(new am4charts.RadarSeries());
+  series.dataFields.valueY = "litres";
+  series.dataFields.categoryX = "Skill";
+  series.name = "Sales";
+  series.strokeWidth = 3;
+  series.zIndex = 2;
+  series.stacked = true;
+
+  let series2 = chart.series.push(new am4charts.RadarColumnSeries());
+  series2.dataFields.valueY = "units";
+  series2.dataFields.categoryX = "Skill";
+  series2.name = "Senior Software Developer";
+  series2.strokeWidth = 0;
+  series2.columns.template.fill = am4core.color("#CDA2AB");
+  series2.columns.template.tooltipText = "Series: {name}\nCategory: {categoryX}\nValue: {valueY}";
+  series2.stacked = true;
+  }
+
+  componentWillUnmount() {
+    if (this.chart) {
+      this.chart.dispose();
+    }
+  }
+
+  render() {
+    return (
+        <Flippy
                 flipOnHover={false} // default false
                 flipOnClick={true} // default false
                 flipDirection="horizontal" // horizontal or vertical
                 ref={(r) => this.flippy = r} // to use toggle method like this.flippy.toggle()
                 // if you pass isFlipped prop component will be controlled component.
                 // and other props, which will go to div
-                style={{ width: '400px', height: '400px' }} /// these are optional style, it is not necessary
+                style={{}} /// these are optional style, it is not necessary
             >
                 <FrontSide
                     style={{
@@ -61,15 +180,9 @@ class Radar extends React.Component {
                 >
                     <div className="row">
                         <Row>
-                            <Col sm="12">
+                            <Col>
                                 <Card>
-                                    <RadarChart
-                                        captions={captions
-                                        }
-                                        data={data
-                                        }
-                                        size={400}
-                                    />
+                                <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div>
                                     <CardBody>
                                     </CardBody>
                                 </Card>
@@ -82,8 +195,8 @@ class Radar extends React.Component {
                     <Profile />
                 </BackSide>
             </Flippy>
-        );
-    }
+      
+    );
+  }
 }
-
 export default Radar;
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -2,40 +2,42 @@ import React from 'react';
 import { Media } from 'reactstrap';
 import Radar from '../components/Radar';
 
-// function Welcome(props) {
-//   return    <div>
-//   <Media className="m-5">
-//   <Media left href="">
-//     <img src="" alt="Profile Picture"/> // get correct db name
-//   </Media>
-//   <Media body>
-//     <Media heading>
-//       {props.name}
-//     </Media>
-//     {props.currentJob}, {props.futureJob}, {props.level}, {props.points}, Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-//   </Media>
-// </Media>
-// </div>
+function Welcome(props) {
+  return <div>
+    <Media className="m-5">
+      <Media left href="/{props.link}">
+        <img src="{props.image}" alt="Profile Picture" /> // get correct db name
+      </Media>
+      <Media body>
+        <Media heading>
+          {props.name}
+        </Media>
+        {props.currentJob}, {props.futureJob}, {props.level}, {props.points}, Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+  </Media>
+    </Media>
+  </div>
 
-// }
+}
 
 
 const UserCard = () => {
   return (
     <div>
-    <Media className="m-5">
-    <Media left href="#">
-      <img src="https://via.placeholder.com/100x100/"/>
-    </Media>
-    <Media body>
-      <Media heading>
-        Media heading
+      <Media className="m-5">
+        <Media left href="#">
+          <img src="https://via.placeholder.com/100x100/" />
+        </Media>
+        <Media body>
+          <Media heading>
+            Media heading
       </Media>
-      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+          Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
     </Media>
-  </Media>
-  <Radar />
-  </div>
+      </Media>
+      <div>
+      <Radar />
+      </div>
+    </div>
   );
 };
 
